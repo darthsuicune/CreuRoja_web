c816cc5a561c9e4b0acf3b602a506cd7cf872a94
Denís Lapuente <lapuente@abbyy.com>
2014-10-01 17:33:35 +0200

    Fix test not passing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe SessionsController do
-	let(:user) { FactoryGirl.create(:user) }
+	let(:user) { FactoryGirl.create(:user, phone: "123") }
 	
 	describe "new" do
 	end
@@ -10,7 +10,7 @@ describe SessionsController do
 		describe "with valid information" do
 			before { post :create, { format: :json, email: user.email, password: user.password } }
 			it "expect response to be a json object" do
-				expect(response.body).to eq({token: user.sessions.last.token}.to_json)
+				expect(response.body).to eq({token: user.sessions.last.token, user: user.for_session}.to_json)
 			end
 		end
 	end
